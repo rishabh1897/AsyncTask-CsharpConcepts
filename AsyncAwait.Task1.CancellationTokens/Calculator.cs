2a4ebd31be7edda120854bff39ee1113933bb921
Rishabh Singh <Rishabh_Singh@epam.com>
2023-04-26 15:51:14 +0530

    Task1 update

--- AsyncAwait.Task1.CancellationTokens/Calculator.cs
+++ AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwait.Task1.CancellationTokens
 {
@@ -6,18 +8,23 @@ namespace AsyncAwait.Task1.CancellationTokens
     internal static class Calculator
     {
         // todo: change this method to support cancellation token
-        public static long Calculate(int n /*, CancellationToken token*/)
+        public static Task<long> Calculate(int n, CancellationToken token)
         {
             long sum = 0;
 
             for (var i = 0; i < n; i++)
             {
+                if (token.IsCancellationRequested)
+                {
+                    throw new OperationCanceledException();
+                }
                 // i + 1 is to allow 2147483647 (Max(Int32)) 
                 sum = sum + (i + 1);
                 Thread.Sleep(10);
+
             }
 
-            return sum;
+            return Task.FromResult(sum);
         }
     }
 }
